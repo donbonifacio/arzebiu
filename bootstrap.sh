2a597bb5ac8a7071e24dd864d50f568767128404
Pedro Santos <donbonifacio@gmail.com>
2012-12-15 12:06:39 +0000

    Improved import

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@ rvm use 1.9.3-jekyll
 sudo apt-get install ruby1.9.1-dev
 gem install jekyll
 gem install hpricot
+gem install sequel
+gem install htmlentities
--- import.rb
+++ import.rb
@@ -1,2 +1,13 @@
-require "jekyll/migrators/wordpressdotcom"
-Jekyll::WordpressDotCom.process("wordpress.xml")
+# From file
+#require "jekyll/migrators/wordpressdotcom"
+#Jekyll::WordpressDotCom.process("wordpress.xml")
+
+
+# from DB
+#require "jekyll/migrators/wordpress"
+#Jekyll::WordPress.process("arzebiu", "root", "", "localhost")
+#
+
+
+require "./lib/wordpress.rb"
+Jekyll::WordPress.process("arzebiu", "root", "", "localhost")
