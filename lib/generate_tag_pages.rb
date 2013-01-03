bf7e6d5bda0aeea9ac5f2dc9c15c1e73ebede080
Pedro Santos <donbonifacio@gmail.com>
2013-01-03 10:26:55 +0000

    Change tags url prefix

--- lib/generate_tag_pages.rb
+++ lib/generate_tag_pages.rb
@@ -9,7 +9,7 @@ class GenerateTagPages
     site.tags.each do |tag_name, tag|
       base_template = File.read('tag.html')
       base_template.gsub!("__page.title__", tag_name)
-      base_template.gsub!("__page.permalink__", "/tags/#{tag_name}")
+      base_template.gsub!("__page.permalink__", "/tag/#{tag_name}")
       base_template.gsub!("__page.slug__", tag_name)
       IO.write("_posts/2000-12-12-#{tag_name}.html", base_template)
       puts "Generated tag page #{tag_name}"
